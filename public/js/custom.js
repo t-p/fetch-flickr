8cadb423e1187d67c8f832dcee00851a01fd9026
Thomas Pfeiffer <thomas@ibpfeiffer.de>
2013-11-17 20:19:16 +0100

    configure fancybox

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
 
   /* Apply fancybox to multiple items */
-  
   $("a[rel=flickr_pic]").fancybox({
-    'transitionIn'  : 'elastic',
-    'transitionOut' : 'elastic',
-    'speedIn'   : 600, 
-    'speedOut'    : 200, 
-    'overlayShow' : false
+    'loop'        : true,
+    'openEffect'  : 'elastic',
+    'closeEffect' : 'elastic',
+    'nextEffect'  : 'elastic',
+    'prevEffect'  : 'elastic'
   });
-  
 });
